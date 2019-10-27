610b8804f7d18086a1bc30cc8727036203768ab4
meganrogge <megrogge@gmail.com>
2019-10-27 17:20:14 -0400

    Revert "Delete app.js"
    This reverts commit cb120bce15ebd1903f59da074b2c59bf5bda5c91.

---
+++ src/app.js
@@ -0,0 +1,28 @@
+import "phaser";
+import { WelcomeScene } from "./welcomeScene";
+import { GameScene } from "./gameScene";
+import { ScoreScene } from "./scoreScene";
+import { ChoiceScene } from "./choiceScene";
+const config = {
+    title: "Runner",
+    type: Phaser.AUTO,
+    width: 1334,
+    height: 750,
+    parent: "game",
+    scene: [WelcomeScene, GameScene, ChoiceScene, ScoreScene],
+    physics: {
+        default: "arcade",
+        arcade: {
+            debug: false
+        }
+    },
+    backgroundColor: "#99badd"
+};
+export class StarfallGame extends Phaser.Game {
+    constructor(config) {
+        super(config);
+    }
+}
+window.onload = () => {
+    var game = new StarfallGame(config);
+};
